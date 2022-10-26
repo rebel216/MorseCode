d31c624982c573a9b9fcbbcedc8c792374ba2702
kumilachew-g <kumilachew1212@gmail.com>
2022-10-26 21:30:21 +0300

    Fix linter errors

--- morse.rb
+++ morse.rb
@@ -33,22 +33,22 @@ def decode_char(code)
         @current_words = word.split(' ')
         @current_words.each do |current_word|
             @current_letter = @dictionary[current_word]
-            print "#{@current_letter}"
+            print '#{@current_letter}'
         end
-        print " "
+        print ' '
     end
 end
 
 # Decode each word
 def word_decoder(word)
     @current_word = word.split.map { |letter| letter_decoder(letter) }.join
-    "#{@current_word} "
+    '#{@current_word} '
 end
 
 # Decode each sentence
 def sentence_decoder(sentence)
     @current_sentence = sentence.split('   ').map { |word| word_decoder(word) }.join
-    print "#{@current_sentence.downcase} \n"
+    print '#{@current_sentence.downcase} \n'
 end
 
 sentence_decoder('-- -.--   -. .- -- .')
