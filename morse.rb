96ecfbdb4dd4558abe6618e5020cd2960586892b
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-10-27 02:39:23 +0530

    Fixed linters

--- morse.rb
+++ morse.rb
@@ -30,16 +30,17 @@
 def decode_char(code)
   @words = code.split
   decode_words(@words)
+  print "\n"
 end
 
-def decode_words()
+def decode_words(words)
   @words.each do |word|
     @current_words = word.split
     decode_sentence(@current_word)
   end
 end
 
-def decode_sentence()
+def decode_sentence(current_word)
   @current_words.each do |current_word|
     @current_letter = @dictionary[current_word]
     print @current_letter.downcase.to_s
