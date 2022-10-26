549d0b2008b2165dea12367e4340cae1e1cec731
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-10-27 02:04:18 +0530

    Final solution

--- morse.rb
+++ morse.rb
@@ -28,31 +28,25 @@
 }
 
 def decode_char(code)
-  @words = code.split
-  @words.each do |word|
-    @current_words = word.split
-    @current_words.each do |current_word|
-      @current_letter = @dictionary[current_word]
-      print @current_letter.to_s
+    @words = code.split('   ')
+    decode_words(@words)   
+end
+
+def decode_words(words)
+    @words.each do |word|
+        @current_words = word.split(' ') 
+        decode_sentence(@current_word)      
     end
-    print ' '
-  end
+    
 end
 
-# Decode each word
-def word_decoder(word)
-  @current_word = word.split.map { |letter| letter_decoder(letter) }.join
-  @current_word.to_s
+def decode_sentence(current_words)
+    @current_words.each do |current_word|
+        @current_letter = @dictionary[current_word]
+        print "#{@current_letter}"
+    end
+    print " "
 end
 
-# Decode each sentence
-def sentence_decoder(sentence)
-  @current_sentence = sentence.split('   ').map { |word| word_decoder(word) }.join
-  print "#{@current_sentence.downcase} \n"
-end
-
-decode_char('-- -.--   -. .- -- .')
-
-decode_char('.-  .-..  .  -..-  .-  -.  -..  .  .-.     ..  ...     ...  ..  -.-.  -.-')
 
 decode_char('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
